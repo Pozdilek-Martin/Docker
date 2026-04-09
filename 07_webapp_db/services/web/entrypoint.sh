d490bc97b34c9fe1e8d45d9b5a614f668c2c6a3e
oooo <pozda2@users.noreply.github.com>
2026-04-09 13:11:07 +0200

    Fix: postgres

--- 07_webapp_db/services/web/entrypoint.sh
+++ 07_webapp_db/services/web/entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-if [ "$DATABASE" = "postgres" ]
-then
+#!/bin/bash
+if [ "$DATABASE" = "postgres" ]; then
     echo "Waiting for postgres..."
 
     while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
@@ -11,7 +10,7 @@ then
 fi
 
 # prvotni vytvoreni a naplneni tabulek
-if [ "$DB_INIT" = "1" ]
+if [ "$DB_INIT" = "1" ];
 then
     echo "Creating the database tables..."
     python manage.py create_db
--- 09_produkce/services/ui/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/app/static"
