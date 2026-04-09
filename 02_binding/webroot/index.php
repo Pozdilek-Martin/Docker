97729c47044e736fe2cb0d419bd2ab59844a461a
oooo <pozda2@users.noreply.github.com>
2026-04-09 08:23:51 +0200

    first commit

---
+++ 02_binding/webroot/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Docker - hello world</title>
+	<style type="text/css">
+	body {
+		background-color: lightsteelblue;
+	}
+	#main-content {
+		margin: 100px;
+		text-align: center;
+	}
+</style>
+</head>
+<body>
+	<div id="main-content">
+		<h1>
+			Hello world
+		</h1>
+	</div>
+</body>
+</html>
